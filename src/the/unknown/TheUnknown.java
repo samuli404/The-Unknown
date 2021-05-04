a7491412eed470e284a5a8cb9f73dd593774d913
samuli404 <Samuli.karkkainen10@gmail.com>
2021-05-04 14:41:54 +0300

    testi

--- src/the/unknown/TheUnknown.java
+++ src/the/unknown/TheUnknown.java
@@ -16,6 +16,7 @@ public class TheUnknown {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("aBahwash");
     }
     
 }
